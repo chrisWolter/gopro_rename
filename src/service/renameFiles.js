b0c1f3d972b277dd8f91b2ecc547e72977ae067e
chriswolter <wolter-christian@outlook.com>
2020-08-03 20:29:18 +0200

    Add progress Bar (WIP)

--- src/service/renameFiles.js
+++ src/service/renameFiles.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
+const fs = require("fs");
+const progress = require("progress-stream");
+const cliProgress = require("cli-progress");
 
 function renameFiles(oldFileName, newFileName) {
   fs.rename(oldFileName, newFileName, (error) => {
@@ -10,12 +12,52 @@ function renameFiles(oldFileName, newFileName) {
   });
 }
 
-function renameAndCopyFiles(dir, oldFile, newFile){
-  if (!fs.existsSync(dir)){
+function renameAndCopyFiles(dir, oldFile, newFile) {
+  if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
-}
-console.log(oldFile);
-console.log(newFile);
-fs.copyFileSync(oldFile, newFile);
-}
+  }
+  console.log(oldFile);
+  console.log(newFile);
+  fs.copyFileSync(oldFile, newFile);
+} /*
+function renameAndCopyFiles(dir, oldFile, newFile) {
+  const bar1 = new cliProgress.SingleBar(
+    {},
+    cliProgress.Presets.shades_classic
+  );
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  bar1.start(100, 0);
+
+  var stat = fs.statSync(oldFile);
+  var str = progress({
+    length: stat.size,
+    time: 100,
+  });
+
+  str.on("progress", function (progress) {
+    bar1.update(progress.percentage);
+  });
+
+  var rd = fs.createReadStream(oldFile);
+  rd.on("error", function (err) {
+    done(err);
+  });
+
+  var wr = fs.createWriteStream(newFile);
+
+  wr.on("error", function (err) {
+    done(err);
+  });
+
+  wr.on("close", function () {
+    done("Finished");
+  });
+
+  rd.pipe(str).pipe(wr);
+} */
+
 module.exports = { renameFiles, renameAndCopyFiles };
