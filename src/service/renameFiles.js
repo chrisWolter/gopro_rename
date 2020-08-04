2d0c77131104c8f3043367b47900f7dc5860fc74
chrisWolter <wolter-christian@outlook.com>
2020-08-04 14:38:30 +0200

    Refatored and removed logger

--- src/service/renameFiles.js
+++ src/service/renameFiles.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
-const progress = require("progress-stream");
-const cliProgress = require("cli-progress");
 
 function renameFiles(oldFileName, newFileName) {
   fs.rename(oldFileName, newFileName, (error) => {
     if (error) {
       new Error(error);
     } else {
-      console.log("renaming: " + oldFileName + " to: " + newFileName);
+      console.log("renamed: " + oldFileName + " to: " + newFileName);
     }
   });
 }
@@ -16,48 +14,7 @@ function renameAndCopyFiles(dir, oldFile, newFile) {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-  console.log(oldFile);
-  console.log(newFile);
   fs.copyFileSync(oldFile, newFile);
-} /*
-function renameAndCopyFiles(dir, oldFile, newFile) {
-  const bar1 = new cliProgress.SingleBar(
-    {},
-    cliProgress.Presets.shades_classic
-  );
-
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-  }
-
-  bar1.start(100, 0);
-
-  var stat = fs.statSync(oldFile);
-  var str = progress({
-    length: stat.size,
-    time: 100,
-  });
-
-  str.on("progress", function (progress) {
-    bar1.update(progress.percentage);
-  });
-
-  var rd = fs.createReadStream(oldFile);
-  rd.on("error", function (err) {
-    console.log(err);
-  });
-
-  var wr = fs.createWriteStream(newFile);
-
-  wr.on("error", function (err) {
-    console.log(err);
-  });
-
-  wr.on("close", function () {
-    console.log("Finished");
-  });
-
-  rd.pipe(str).pipe(wr);
-} */
+} 
 
 module.exports = { renameFiles, renameAndCopyFiles };
